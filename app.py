08ea97a69706107dff680325b5bde8c08fa1538c
Elvira Gandelman <elvirag@gmail.com>
2020-06-19 19:56:46 +0300

    changed to cfg, optimised imports (doesn't appear to improve performance)

--- app.py
+++ app.py
@@ -1,28 +1,28 @@
-import configparser
 import logging
-import os
-import subprocess
 import tkinter as tk
+from configparser import ConfigParser
+from os import system
+from subprocess import Popen, PIPE
 from time import sleep
 
 from db import actions
 
 
 def get_pid_application():
-	command = f"Get-Process {config['basic']['PROCESS']}"
+	command = f"Get-Process {cfg['PROCESS']}"
 	command_format = "Format-Table Id -HideTableHeaders -ErrorAction SilentlyContinue"
 
-	p = subprocess.Popen(["powershell.exe", command, "|", command_format], stdout=subprocess.PIPE)
+	p = Popen(["powershell.exe", command, "|", command_format], stdout=PIPE)
 	application_pid = p.communicate()[0].strip().decode("utf-8")
 
 	return application_pid
 
 
 def measure_curr_application_time():
-	command = f"New-TimeSpan -Start (get-process {config['basic']['PROCESS']}).StartTime"
+	command = f"New-TimeSpan -Start (get-process {cfg['PROCESS']}).StartTime"
 	command_format = f"Format-Table Hours, Minutes -HideTableHeaders -ErrorAction SilentlyContinue"
 
-	p = subprocess.Popen(["powershell.exe", command, "|", command_format], stdout=subprocess.PIPE)
+	p = Popen(["powershell.exe", command, "|", command_format], stdout=PIPE)
 	time = p.communicate()[0]
 	try:
 		hours, minutes = [int(item) for item in time.strip().split()]
@@ -35,16 +35,16 @@ def measure_curr_application_time():
 def counter_label(root, label, i):
 	if i > 0:
 		i -= 1
-		label.config(text=f"{config['basic']['PROCESS_NAME']} will close in {i} seconds")
-		root.after(config['basic'].getint('MAX_OPERATION_TIME') * 100, lambda: counter_label(root, label, i))
+		label.config(text=f"{cfg['PROCESS_NAME']} will close in {i} seconds")
+		root.after(cfg.getint('MAX_OPERATION_TIME') * 100, lambda: counter_label(root, label, i))
 	else:
 		root.destroy()
 
 
 def close_application(time):
-	closing_message_start = f"Sorry, you've been using {config['basic']['PROCESS_NAME']} way too much...\n"
-	closing_message_end = f"It's been {time}min, your limit is: {config['basic'].getint('MAX_OPERATION_TIME')}min!"
-	closing_title = f"{config['basic']['PROCESS_NAME']} will close now."
+	closing_message_start = f"Sorry, you've been using {cfg['PROCESS_NAME']} way too much...\n"
+	closing_message_end = f"It's been {time}min, your limit is: {cfg.getint('MAX_OPERATION_TIME')}min!"
+	closing_title = f"{cfg['PROCESS_NAME']} will close now."
 
 	root = tk.Tk()
 	root.attributes("-topmost", True)
@@ -54,15 +54,15 @@ def close_application(time):
 	message = closing_message_start + closing_message_end
 	message = tk.Message(root, text=message, width=300, padx=20, pady=15)
 	message.pack()
-	counter_label(root, label, config['basic'].getint('TIME_TO_DESTROY'))
+	counter_label(root, label, cfg.getint('TIME_TO_DESTROY'))
 	button = tk.Button(root, text='Close', width=25, command=root.destroy)
 	button.pack()
 	root.mainloop()
-	os.system(f"taskkill /im {config['basic']['PROCESS']}.exe")
+	system(f"taskkill /im {cfg['PROCESS']}.exe")
 
 
 def check_max_time(time):
-	if time > config['basic'].getint('MAX_OPERATION_TIME'):
+	if time > cfg.getint('MAX_OPERATION_TIME'):
 		close_application(time)
 
 
@@ -73,8 +73,9 @@ def update_app_time():
 
 
 def check_app_status():
-	command = f"get-process {config['basic']['PROCESS']} -ErrorAction SilentlyContinue"
-	p = subprocess.Popen(["powershell.exe", command], stdout=subprocess.PIPE)
+	command = f"get-process {cfg['PROCESS']}"
+	command_format = "-ErrorAction SilentlyContinue"
+	p = Popen(["powershell.exe", command, command_format], stdout=PIPE)
 	output = p.communicate()[0]
 	return output
 
@@ -85,20 +86,21 @@ def main():
 		try:
 			is_running = check_app_status()
 			if not is_running:
-				logging.info(f"sleeping for {config['basic']['SLEEP_TIME']} seconds")
-				sleep(config['basic'].getint('SLEEP_TIME'))
+				logging.info(f"sleeping for {cfg['SLEEP_TIME']} seconds")
+				sleep(cfg.getint('SLEEP_TIME'))
 				continue
 			update_app_time()
 			total_time = actions.get_total_time()
 			check_max_time(total_time)
-			sleep(config['basic'].getint('SLEEP_TIME'))
+			sleep(cfg.getint('SLEEP_TIME'))
 		except Exception as e:
 			print(e)
 
 
 if __name__ == "__main__":
-	config = configparser.ConfigParser()
+	config = ConfigParser()
 	config.read('config.ini')
+	cfg = config['basic']
 	message_format = '%(asctime)s %(levelname)s %(message)s'
-	logging.basicConfig(filename=f"{config['basic']['PROCESS']}_log.log", level=logging.DEBUG, format=message_format)
+	logging.basicConfig(filename=f"{cfg['PROCESS']}_log.log", level=logging.DEBUG, format=message_format)
 	main()
